44ca065bc515f307e4707913b81705c0a340a97e
ijanggmu <sthaejan00@gmail.com>
2022-04-06 14:55:25 +0545

    Initial Commit

---
+++ DataAccess.EFCore/ApplicationContext.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Entities; //reference of domain entities
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace DataAccess.EFCore
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Developer> Developers { get; set; }
+        public DbSet<Project> Projects { get; set; }
+    }
+}
+        
